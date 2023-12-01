1dbb20ba6b10c149c3ffb12edec9d9c71443632e
unknown <arfinpeul523@gmail.com>
2023-12-01 20:05:29 +0600

    add navbar

--- src/Root/Root.jsx
+++ src/Root/Root.jsx
@@ -1,9 +1,11 @@
 import { Outlet } from "react-router-dom";
 import Footer from "../Pages/Shared/Footer";
+import Navbar from "../Pages/Shared/Navbar";
 
 const Root = () => {
   return (
-    <div className="font-Rubik">
+    <div className="font-Rubik ">
+      <Navbar></Navbar>
       <Outlet></Outlet>
       <Footer></Footer>
     </div>
