acb0af6a2123982c2ee14bde47be93953ec50aa6
unknown <arfinpeul523@gmail.com>
2023-12-01 20:46:27 +0600

    wishlist page add

--- src/Pages/Wishlist/Wishlist.jsx
+++ src/Pages/Wishlist/Wishlist.jsx
@@ -1,7 +1,10 @@
 const Wishlist = () => {
   return (
-    <div>
-      <h1>Wishlist</h1>
+    <div className="lg:my-32 md:my-20 my-5">
+      <h1 className="text-4xl font-medium">Wishlist</h1>
+      <p className="text-xl text-center py-5">
+        Your wishlist doesn t have any items! Start Shopping
+      </p>
     </div>
   );
 };
