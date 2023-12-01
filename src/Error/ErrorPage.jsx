6927db61761780d4b68dcea00b36479e6a43cd59
unknown <arfinpeul523@gmail.com>
2023-12-01 14:09:26 +0600

    ser error page

--- src/Error/ErrorPage.jsx
+++ src/Error/ErrorPage.jsx
@@ -6,7 +6,7 @@ const ErrorPage = () => {
       <div className="text-center">
         <h1 className="text-9xl font-bold">404</h1>
         <h3 className="my-5 text-4xl font-semibold">Page Not found</h3>
-        <p className=" inline py-2 px-4  rounded-full cursor-pointer">
+        <p className=" inline py-2 px-4 bg-custom_blue hover:bg-custom_yellow hover:text-custom_white  rounded-full cursor-pointer">
           <Link to="/">Back to Home</Link>
         </p>
       </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,14 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    colors: {
+      custom_blue: "#1fb6ff",
+      custom_yellow: "#fed700",
+      custom_white: "#FFFFFF",
+      custom_black: "#303030",
+      custom_gray_text: "#c8c8c8",
+      custom_gray_btn: "#eef0f1",
+    },
     extend: {},
   },
   plugins: [],
