f8e5625dd5f2df844a6eb3963d4d12c94a5a2b72
unknown <arfinpeul523@gmail.com>
2023-12-01 23:22:42 +0600

    set register page

--- src/Pages/Register/Register.jsx
+++ src/Pages/Register/Register.jsx
@@ -1,7 +1,74 @@
+import { Link } from "react-router-dom";
+
 const Register = () => {
   return (
     <div>
-      <h1>register</h1>
+      <form className="card-body lg:w-1/2 mx-auto lg:mt-14">
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text">First Name</span>
+          </label>
+          <input
+            type="text"
+            placeholder="First Name"
+            className="input input-bordered"
+            required
+            name="first_name"
+          />
+        </div>
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text">Last Name</span>
+          </label>
+          <input
+            type="text"
+            placeholder="Last Name"
+            className="input input-bordered"
+            required
+            name="last_name"
+          />
+        </div>
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text">E-Mail</span>
+          </label>
+          <input
+            type="email"
+            placeholder="E-Mail"
+            className="input input-bordered"
+            required
+            name="email"
+          />
+        </div>
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text">Password</span>
+          </label>
+          <input
+            type="password"
+            placeholder="Password"
+            className="input input-bordered"
+            required
+            name="password"
+          />
+          <label className="label">
+            <a href="#" className="label-text-alt link link-hover">
+              Forgot password?
+            </a>
+          </label>
+        </div>
+        <div className="form-control mt-6">
+          <button className="btn bg-custom_yellow hover:bg-custom_blue hover:text-custom_white">
+            Register
+          </button>
+          <p className="py-2">
+            Already have an account?{" "}
+            <Link to="/login" className="hover:text-custom_blue">
+              Login Here
+            </Link>
+          </p>
+        </div>
+      </form>
     </div>
   );
 };
