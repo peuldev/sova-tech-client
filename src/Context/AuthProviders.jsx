e3aac9ed25574fa115e67b1437d53a7685683fdd
unknown <arfinpeul523@gmail.com>
2023-12-02 23:18:17 +0600

    user auth state set

--- src/Context/AuthProviders.jsx
+++ src/Context/AuthProviders.jsx
@@ -1,12 +1,25 @@
-import { createContext, useState } from "react";
-import { createUserWithEmailAndPassword } from "firebase/auth";
+import { createContext, useEffect, useState } from "react";
+import {
+  createUserWithEmailAndPassword,
+  onAuthStateChanged,
+} from "firebase/auth";
 import auth from "../Services/Authentication/Authentication";
 export const AuthContext = createContext(null);
 const AuthProviders = ({ children }) => {
-  const { user, setUser } = useState(null);
+  const [user, setUser] = useState(null);
   const createUser = (email, password) => {
     return createUserWithEmailAndPassword(auth, email, password);
   };
+
+  useEffect(() => {
+    const unSubscribe = onAuthStateChanged(auth, (currentUser) => {
+      console.log("user auth changed", currentUser);
+      setUser(currentUser);
+    });
+    return () => {
+      unSubscribe();
+    };
+  }, []);
   const userInfo = { user, createUser };
   return (
     <AuthContext.Provider value={userInfo}>{children}</AuthContext.Provider>
