9127cc7b0ff37fc3bc2ee55055ae8de9162a38b6
unknown <arfinpeul523@gmail.com>
2023-12-05 15:42:39 +0600

    update product details

--- src/Pages/Watch/ProductDetails.jsx
+++ src/Pages/Watch/ProductDetails.jsx
@@ -10,16 +10,25 @@ const ProductDetails = () => {
       <h1 className="text-center text-custom_blue text-4xl py-5 underline">
         BASIC INFORMATION
       </h1>
-      <div className="lg:flex md:flex">
-        <img className="rounded" src={photo} alt="" />
-        <div className="lg:ml-10 ml-2 mt-5 space-y-5">
-          <h1 className="lg:text-3xl text-xl">{title}</h1>
-          <p className="lg:text-2xl text-xl">Stock Status: {stock}</p>
-          <p className="lg:text-2xl text-xl">Product code: {code}</p>
-          <p className="lg:text-2xl text-xl">Brand: {brand}</p>
-          <p className="lg:text-2xl text-xl">Color: {color}</p>
-          <p className="lg:text-2xl text-xl">Price: {price} $</p>
+      <div className="lg:flex md:flex ">
+        <img className="rounded  w-1/4 h-1/4" src={photo} alt="" />
+        <div className="lg:ml-20 ml-2 mt-5 space-y-1 lg:py-5">
+          <h1 className="text-custom_blue lg:text-3xl text-xl">{title}</h1>
+          <p className="lg:text-xl text-xl">Stock Status: <span className="text-custom_red">{stock}</span></p>
+          <p className="lg:text-xl text-xl">Product code: {code}</p>
+          <p className="lg:text-xl text-xl">Brand: {brand}</p>
+          <p className="lg:text-xl text-xl">Color: {color}</p>
+          <p className="lg:text-xl text-xl font-semibold text-custom_red">
+            $ {price}
+          </p>
+          <p className="  bg-custom_gray cursor-pointer rounded hover:bg-custom_yellow hover:text-custom_white px-2 py-2 tooltip">
+            ADD TO CART
+          </p>
         </div>
+      </div>
+      <div className="border-t-2 border-t-custom_gray_color ">
+        <h1 className="lg:text-2xl font-semibold lg:py-4">Description</h1>
+        <p>{details}</p>
       </div>
     </div>
   );
