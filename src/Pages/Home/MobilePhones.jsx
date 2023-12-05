316a5580abb15109bdb949bcc0e50853aa8939b2
unknown <arfinpeul523@gmail.com>
2023-12-05 15:02:17 +0600

    use tooltip

--- src/Pages/Home/MobilePhones.jsx
+++ src/Pages/Home/MobilePhones.jsx
@@ -15,16 +15,25 @@ const MobilePhones = ({ product }) => {
           $ <span className="font-semibold text-custom_red"> {price}</span>{" "}
         </p>
         <div className="flex items-center pb-5 ">
-          <p className="  bg-custom_gray cursor-pointer rounded hover:bg-custom_yellow hover:text-custom_white px-2 py-2">
+          <p
+            className="  bg-custom_gray cursor-pointer rounded hover:bg-custom_yellow hover:text-custom_white px-2 py-2 tooltip"
+            data-tip="Add to Cart"
+          >
             ADD TO CART
           </p>
           <Link to={`/product/${_id}`}>
-            <p className=" cursor-pointer hover:bg-custom_yellow   hover:text-custom_white px-2 py-2 rounded">
+            <p
+              className=" cursor-pointer hover:bg-custom_yellow   hover:text-custom_white px-2 py-2 rounded tooltip"
+              data-tip="View Details"
+            >
               <TbListDetails />
             </p>
           </Link>
           <Link to={`/updateproduct/${_id}`}>
-            <p className="cursor-pointer hover:bg-custom_yellow  hover:text-custom_white px-2 py-2 rounded">
+            <p
+              className="cursor-pointer hover:bg-custom_yellow  hover:text-custom_white px-2 py-2 rounded tooltip"
+              data-tip="Update"
+            >
               <RxUpdate />
             </p>
           </Link>
