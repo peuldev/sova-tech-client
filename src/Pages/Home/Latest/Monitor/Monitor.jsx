837e31394c687578dff9264d760df4380c840b25
peuldev <arfinpeul523@gmail.com>
2024-01-15 21:57:07 +0600

    get monitor data

--- src/Pages/Home/Latest/Monitor/Monitor.jsx
+++ src/Pages/Home/Latest/Monitor/Monitor.jsx
@@ -1,6 +1,16 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const Monitor = () => {
+  const [monitor, setMonitor] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:5000/device")
+      .then((res) => res.json())
+      .then((data) => {
+        const monitors = data.filter((item) => item.category === "monitor");
+        setMonitor(monitors);
+      });
+  }, []);
+  console.log(monitor);
   return <div>Monitor</div>;
 };
 
