69dbcf2015f2a72ece386735cce713102da06ca2
peuldev <arfinpeul523@gmail.com>
2024-01-16 17:04:16 +0600

    add to cart data sent data base

--- src/Pages/Home/Latest/Keyboard/ShowKeyboard.jsx
+++ src/Pages/Home/Latest/Keyboard/ShowKeyboard.jsx
@@ -1,8 +1,30 @@
 import React from "react";
 import { TbListDetails } from "react-icons/tb";
 import { Link } from "react-router-dom";
+import Swal from "sweetalert2";
 
 const ShowKeyboard = ({ keyboard }) => {
+  const handleAddToCart = (carts) => {
+    fetch("http://localhost:5000/carts", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(carts),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.insertedId) {
+          Swal.fire({
+            title: "Sova Tech",
+            text: "Product Add Successfully",
+            icon: "success",
+            showConfirmButton: false,
+            timer: 1500,
+          });
+        }
+      });
+  };
   const { _id, title, photo, price } = keyboard;
   return (
     <div className="border border-[#DFCCFB] rounded-t">
@@ -15,7 +37,10 @@ const ShowKeyboard = ({ keyboard }) => {
           $ <span className="font-semibold text-[#FF004D]"> {price}</span>{" "}
         </p>
         <div className="flex items-center">
-          <p className="cursor-pointer border border-[#DFCCFB] hover:bg-[#DFCCFB] hover:text-custom_white rounded px-2">
+          <p
+            onClick={() => handleAddToCart(keyboard)}
+            className="cursor-pointer border border-[#DFCCFB] hover:bg-[#DFCCFB] hover:text-custom_white rounded px-2"
+          >
             ADD TO CART
           </p>
           <Link to={`/details/${_id}`}>
